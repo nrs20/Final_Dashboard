36434b34196ba482d1918621c95c91c3c13f80e1
nrs20 <nrs5@njit.edu>
2024-05-06 21:13:56 -0400

    i2

--- app/src/child1.js
+++ app/src/child1.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import * as d3 from "d3";
 class Child1 extends Component {
   constructor(props) {
+    
     super(props);
     this.state = {
       x_scale: 10,
